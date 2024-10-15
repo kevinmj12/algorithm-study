43526f3d694ac8264f9ceaa6f47b003b842805d1
김민제 <55120784+kevinmj12@users.noreply.github.com>
2024-10-15 16:54:51 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,20 @@
+-- 코드를 작성해주세요
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPERS
+WHERE (   
+    SKILL_CODE & (
+        SELECT CODE
+        FROM SKILLCODES
+        WHERE NAME IN ("Python")
+    ) = (SELECT CODE
+        FROM SKILLCODES
+        WHERE NAME IN ("Python")) OR
+    SKILL_CODE & (
+        SELECT CODE
+        FROM SKILLCODES
+        WHERE NAME IN ("C#")
+    ) = (SELECT CODE
+        FROM SKILLCODES
+        WHERE NAME IN ("C#"))
+)
+ORDER BY ID ASC;
