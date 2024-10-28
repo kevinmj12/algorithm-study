e208c901fe64a2b560c2b5c77b6d6bc18f6b90be
김민제 <55120784+kevinmj12@users.noreply.github.com>
2024-10-28 18:07:25 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 112 ms, Memory: 44072 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool answer[6562][6562];
+
+void solve(int n, int y, int x){
+    if (n == 3){
+        answer[y][x] = true;
+        answer[y][x+1] = true;
+        answer[y][x+2] = true;
+        answer[y+1][x] = true;
+        // 빈 칸
+        answer[y+1][x+2] = true;
+        answer[y+2][x] = true;
+        answer[y+2][x+1] = true;
+        answer[y+2][x+2] = true;
+    }
+    else{
+        solve(n/3, y, x);
+        solve(n/3, y, x + (n/3));
+        solve(n/3, y, x + (2*n/3));
+        solve(n/3, y + (n/3), x);
+        // 가운데 빈 칸
+        solve(n/3, y + (n/3), x + (2*n/3));
+        solve(n/3, y + (2*n/3), x);
+        solve(n/3, y + (2*n/3), x + (n/3));
+        solve(n/3, y + (2*n/3), x + (2*n/3));
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    solve(n, 0, 0);
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if (answer[i][j]){
+                cout << "*";
+            }
+            else{
+                cout << " ";
+            }
+        }
+        cout << "\n";
+    }
+}
