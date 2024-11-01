453e6be64f31bf67c04ce9ebe6ae7fcb9ac1b5d2
김민제 <55120784+kevinmj12@users.noreply.github.com>
2024-11-01 23:43:41 +0900

    [level 3] Title: 등산코스 정하기, Time: 5684.21 ms, Memory: 51.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.cpp
@@ -0,0 +1,120 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <set>
+
+#define MAX_S 50001
+#define MAX_I 10000001
+
+
+using namespace std;
+
+set<int> summitsSet;
+int answerS = 0;
+int answerI = MAX_I;
+int djIntensity[MAX_S];
+
+vector<pair<int, int>> graph[MAX_S];
+vector<int> connectedSpots;
+bool visited[MAX_S];
+
+void reset(int n){
+    for (int i = 1; i <= n; i++){
+        visited[i] = false;
+        djIntensity[i] = MAX_I;
+    }
+    connectedSpots.clear();
+}
+
+bool isSummit(int spot){
+    if (summitsSet.find(spot) != summitsSet.end()){
+        return true;
+    }
+    return false;
+}
+
+pair<int, int> findMinIntensity(int n, int spot){
+    int minIntensity = MAX_I;
+    int minSpot;
+    
+    for (int i = 0; i < connectedSpots.size(); i++){
+        int nextS = connectedSpots[i];
+        int nextI = djIntensity[nextS];
+        
+        if (!visited[nextS] && nextI < minIntensity){
+            minIntensity = nextI;
+            minSpot = nextS;
+        }
+    }
+    if (minIntensity == MAX_I){
+        return {-1, -1};
+    }
+    return {minSpot, minIntensity};
+}
+
+void updateIntensity(int spot, int intensity){
+    for (int i = 0; i < graph[spot].size(); i++){
+        int nextS = graph[spot][i].first;
+        int nextI = graph[spot][i].second;
+        
+        if (djIntensity[nextS] == MAX_I){
+            connectedSpots.push_back(nextS);
+        }
+        djIntensity[nextS] = min(djIntensity[nextS], max(intensity, nextI));
+    }
+}
+
+void solve(int n, int gate){
+    while (true){
+        pair<int, int> next = findMinIntensity(n, gate);
+        int nextSpot = next.first;
+        int nextIntensity = next.second;
+
+        if (nextSpot == -1 || nextIntensity > answerI){
+            return;
+        }
+        
+        visited[nextSpot] = true;
+        
+        if (isSummit(nextSpot)){
+            if (nextIntensity < answerI || (nextIntensity == answerI && nextSpot < answerS)){
+                answerI = nextIntensity;
+                answerS = nextSpot;
+            }
+            continue;
+        }
+        
+        updateIntensity(nextSpot, nextIntensity);
+    }
+}
+
+vector<int> solution(int n, vector<vector<int>> paths, vector<int> gates, vector<int> summits) {
+    vector<int> answer;
+    
+    for (int path = 0; path < paths.size(); path++){
+        int i = paths[path][0];
+        int j = paths[path][1];
+        int w = paths[path][2];
+        graph[i].push_back({j, w});
+        graph[j].push_back({i, w});
+    }
+    
+    for (int i = 0; i < summits.size(); i++){
+        summitsSet.insert(summits[i]);
+    }
+    
+    for (int i = 0; i < gates.size(); i++){
+        reset(n);        
+        int gate = gates[i];
+        visited[gate] = true;
+        
+        updateIntensity(gate, 0);
+        solve(n, gate);
+    }
+    
+    answer.push_back(answerS);
+    answer.push_back(answerI);
+    
+    return answer;
+}
