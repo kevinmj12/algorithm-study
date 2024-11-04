762588bf10b0f95e4dfd7845c28d7bdba8172ed9
김민제 <55120784+kevinmj12@users.noreply.github.com>
2024-11-04 18:43:59 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1408 ms, Memory: 59008 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cc
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define MAX_N 100001
+
+vector<vector<int>> graph;
+int root[MAX_N];
+pair<int, int> costs[MAX_N];
+
+int Find(int a){
+    if (a == root[a]){
+        return a;
+    }
+    return root[a] = Find(root[a]);
+}
+
+void Union(int a, int b){
+    a = Find(a);
+    b = Find(b);
+
+    if (a < b){
+        root[b] = a;
+    }
+    else{
+        root[a] = b;
+    }
+}
+
+int main(){
+    // ios::sync_with_stdio(false);
+    // cin.tie(NULL); cout.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++){
+        int a, b, c;
+        cin >> a >> b >> c;
+        graph.push_back({c, a, b});
+    }
+    sort(graph.begin(), graph.end());
+
+    for (int i = 1; i <= n; i++){
+        root[i] = i;
+    }
+
+    int totalCosts = 0;
+    int paths = 0;
+    for (int i = 0; i < m; i++){
+        if (paths >= n-2){
+            break;
+        }
+
+        int c = graph[i][0];
+        int a = graph[i][1];
+        int b = graph[i][2];
+
+        if (Find(a) == Find(b)){
+            continue;
+        }
+        Union(a, b);
+        paths++;
+
+        totalCosts += c;
+        // costs[a].first += c;
+        // costs[a].second++;
+        // costs[b].first += c;
+        // costs[b].second++;
+    }
+
+    // int maxCost = 0;
+    // for (int i = 1; i <= n; i++){
+    //     if (costs[i].second <= 2){
+    //         maxCost = max(maxCost, costs[i].first);
+    //     }
+    // }
+
+    cout << totalCosts;
+}
