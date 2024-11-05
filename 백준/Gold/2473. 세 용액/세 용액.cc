6875bdc99587be90f85c06977b8976f12d75860e
김민제 <55120784+kevinmj12@users.noreply.github.com>
2024-11-05 15:30:23 +0900

    [Gold III] Title: 세 용액, Time: 28 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.cc
+++ 백준/Gold/2473. 세 용액/세 용액.cc
@@ -5,26 +5,6 @@
 using namespace std;
 
 vector<long long> liquids;
-vector<pair<int, int>> twoLiquids;
-
-int binarySearch(int n, long long target){
-    int left = 0;
-    int right = n-1;
-    target *= -1;
-    while (left <= right){
-        int mid = left + (right - left) / 2;
-        if (liquids[mid] == target){
-            return mid;
-        }
-        if (liquids[mid] < target){
-            left = mid+1;
-        }
-        else {
-            right = mid-1;
-        }
-    }
-    return left;
-}
 
 int main(){
     int n;
@@ -38,38 +18,29 @@ int main(){
 
     sort(liquids.begin(), liquids.end());
 
-    for (int i = 0; i < n-1; i++){
-        for (int j = i+1; j < n; j++){
-            twoLiquids.push_back({i, j});
-        }
-    }
 
     long long answerVal = 3000000000;
     vector<int> answer;
 
-    for (int i = 0; i < twoLiquids.size(); i++){
-        int a = twoLiquids[i].first;
-        int b = twoLiquids[i].second;
-        long long val = liquids[a]+liquids[b];
+    for (int i = 0; i < liquids.size()-2; i++){
+        int left = i+1;
+        int right = n-1;
 
-        int trg = binarySearch(n, val);
-        
-        if ((trg < n && trg != a && trg != b)){
-            if (answerVal > abs(val + liquids[trg])){
-                answerVal = abs(val + liquids[trg]);
-                answer = {a, b, trg};
+        while (left < right){
+            long long val = liquids[i] + liquids[left] + liquids[right];
+            if (answerVal > abs(val)){
+                answerVal = abs(val);
+                answer = {i, left, right};
             }
-        }
-        trg--;
-        if ((trg >= 0 && trg != a && trg != b)){
-            if (answerVal > abs(val + liquids[trg])){
-                answerVal = abs(val + liquids[trg]);
-                answer = {a, b, trg};
+
+            if (val < 0){
+                left++;
+            }
+            else{
+                right--;
             }
         }
     }
-
-    sort(answer.begin(), answer.end());
 
     for (int i = 0; i < answer.size(); i++){
         cout << liquids[answer[i]] << " ";
