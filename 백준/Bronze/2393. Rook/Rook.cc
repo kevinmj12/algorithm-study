b8316b9838b0d89ee4cd9b4c0a28d25c2798db25
김민제 <55120784+kevinmj12@users.noreply.github.com>
2025-09-23 23:01:25 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string rook = R"(  ___  ___  ___
+  | |__| |__| |
+  |           |
+   \_________/
+    \_______/
+     |     |
+     |     |
+     |     |
+     |     |
+     |_____|
+  __/       \__
+ /             \
+/_______________\)";
+
+    cout << rook;
+}
