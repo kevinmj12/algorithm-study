05f1eb83d5c7957d9ec3301042e6391557746e85
김민제 <55120784+kevinmj12@users.noreply.github.com>
2025-09-26 15:37:20 +0900

    [Silver III] Title: 어린 왕자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isInside(int x, int y, int cx, int cy, int r){
+    int distance = pow(x-cx, 2) + pow(y-cy, 2);
+    if (distance < r*r){
+        return true;
+    }
+    return false;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int T;
+    cin >> T;
+
+    while (T--){
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        int n;
+        cin >> n;
+
+        int answer = 0;
+        for (int i = 0; i < n; i++){
+            int cx, cy, r;
+            cin >> cx >> cy >> r;
+
+            bool startInside = isInside(x1, y1, cx, cy, r);
+            bool endInside = isInside(x2, y2, cx, cy, r);
+
+            if ((startInside || endInside) && !(startInside && endInside)){
+                answer++;
+            }
+        }
+
+        cout << answer << "\n";
+    }
+}
