c2231a32a7d622c25cb338147cf3a736bd27c411
김민제 <55120784+kevinmj12@users.noreply.github.com>
2025-10-25 18:35:46 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 8.11 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
@@ -1,38 +1,93 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 #include <iostream>
 
 using namespace std;
 
-bool isPrime(long long num) {
-    if (num < 2) return false;
-    for (long long i = 2; i * i <= num; i++) {
-        if (num % i == 0) return false;
+string decToK(int n, int k){
+    string rtn = "";
+    while (n > k){
+        rtn += (n % k) + '0';
+        n /= k;
+    }
+    rtn += n + '0';
+    reverse(rtn.begin(), rtn.end());
+    
+    return rtn;
+}
+
+bool isPrime(long long p){
+    if (p == 1){
+        return false;
+    }
+    for (long long i = 2; i*i <= p; i++){
+        if ((p % i) == 0){
+            return false;
+        }
     }
     return true;
 }
 
 int solution(int n, int k) {
     int answer = 0;
-
-    string pStr = "";
-    while (n > 0) {
-        pStr += to_string(n % k);
-        n /= k;
-    }
-    reverse(pStr.begin(), pStr.end());
-
-    int left = 0;
-    for (int i = 0; i <= pStr.length(); i++) {
-        if (i == pStr.length() || pStr[i] == '0') {
-            if (i > left) {
-                long long p = stoll(pStr.substr(left, i - left));
-                if (isPrime(p)) answer++;
+    
+    string nk = decToK(n, k);
+    
+    // int maxP = 0;
+    string strP = "";
+    vector<long long> pVec;
+    
+    for (int i = 0; i < nk.length(); i++){
+        if (nk[i] != '0'){
+            strP += nk[i];
+        }
+        else{
+            if (strP.length()){
+                long long p = stoll(strP);
+                pVec.push_back(p);
+                // maxP = max(maxP, p);
             }
-            left = i + 1;
+            strP = "";
         }
     }
-
+    
+    if (strP.length()){
+        long long p = stoll(strP);
+        pVec.push_back(p);
+        // maxP = max(maxP, p);
+    }
+    
+    // maxP까지 소수 판별
+//     vector<bool> isPrime(maxP+1, true);
+//     isPrime[1] = false;
+    
+//     for (int i = 2; i*i <= maxP; i++){
+//         if (isPrime[i] == true){
+//             int ii = i + i;
+//             while (ii <= maxP){
+//                 isPrime[ii] = false;
+//                 ii += i;
+//             }
+//         }
+//         else{
+//             continue;
+//         }
+//     }
+    
+//     for (int p: pVec){
+//         if (isPrime[p]){
+//             answer++;
+//         }
+//     }
+    
+    // cout << nk << "\n";
+    for (long long p: pVec){
+        if (isPrime(p)){
+            answer++;
+        }
+    }
+    
     return answer;
 }
