3239a821e49cb91dfde5634ae66b551809649c1c
김민제 <55120784+kevinmj12@users.noreply.github.com>
2025-11-05 22:10:35 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2252 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -1,45 +1,49 @@
 #include <iostream>
-#include <queue>
+#include <deque>
+#include <climits>
 using namespace std;
 #define MAX 100001
+
 bool visited[MAX];
 
 int main(){
     int N, K;
     cin >> N >> K;
 
-    priority_queue<pair<int ,int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-    pq.push({0, N});
-
     int answer = 0;
-
     if (N >= K){
         answer = N-K;
     }
     else{
+        deque<pair<int ,int>> dq;
+        dq.push_back({N, 0});
+
         while (true){
-            int val = pq.top().first;
-            int cur = pq.top().second;
-            pq.pop();
-    
+            int cur = dq.front().first;
+            int val = dq.front().second;
+            dq.pop_front();
+
+            if (visited[cur]){
+                continue;
+            }
             visited[cur] = true;
+
             if (cur == K){
                 answer = val;
                 break;
             }
-
             if (cur*2 < MAX && !visited[cur*2]){
-                pq.push({val, cur*2});
+                dq.push_front({cur*2, val});
             }
             if (cur > 0 && !visited[cur-1]){
-                pq.push({val+1, cur-1});
+                dq.push_back({cur-1, val+1});
             }
-            if (cur < MAX && !visited[cur+1]){
-                pq.push({val+1, cur+1});
+            if (cur+1 < MAX && !visited[cur+1]){
+                dq.push_back({cur+1, val+1});
             }
         }
     }
+
     cout << answer;
     return 0;
 }
