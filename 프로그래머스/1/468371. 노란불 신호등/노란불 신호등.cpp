1cf87c1f005b6a14db8330ccbecb2f13d31c6935
김민제 <55120784+kevinmj12@users.noreply.github.com>
2026-04-20 17:56:01 +0900

    [level 1] Title: 노란불 신호등, Time: 13.20 ms, Memory: 3.69 MB -BaekjoonHub

---
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<vector<int>> signals) {
+    int answer = -1;
+    
+    int t = 0;
+    int maxT = 1;
+    vector<int> nextChange(signals.size());
+    vector<int> status(signals.size());
+    
+    
+    for (int i = 0; i < signals.size(); i++){
+        maxT *= (signals[i][0] + signals[i][1] + signals[i][2]);
+        nextChange[i] = signals[i][0];
+    }
+    
+    for (int t = 1; t <= maxT; t++){
+        for (int i = 0; i < signals.size(); i++){
+            if (t == nextChange[i]){
+                status[i] = (status[i] + 1) % 3;
+                nextChange[i] += signals[i][status[i]];
+            }
+        }
+        bool allY = true;
+        for (int i = 0; i < signals.size(); i++){
+            if (status[i] != 1){
+                allY = false;
+                break;
+            }
+        }
+        
+        if (allY){
+            answer = t+1;
+            break;
+        }
+    }
+    
+    return answer;
+}
